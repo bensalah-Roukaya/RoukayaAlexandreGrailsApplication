69fe6d3a2a2e3abdae4933aa81e8f0216523ebd3
AlexandreTrujillo <alexandre.trujillo@etu.univ-cotedazur.fr>
2019-10-03 11:07:46 +0200

    get annonce/id & get annonces

---
+++ grails-app/controllers/roukayaalexandregrailsapplication/ApiController.groovy
@@ -0,0 +1,58 @@
+package roukayaalexandregrailsapplication
+
+import grails.converters.JSON
+import grails.converters.XML
+
+class ApiController {
+
+    // GET / PUT / PATCH / DELETE
+    def annonce() {
+        switch(request.getMethod())
+        {
+            case "GET":
+                if (!params.id)
+                    return response.status = 400
+                def annonceInstance = Annonce.get(params.id)
+                if (!annonceInstance)
+                    return response.status = 404
+                response.withFormat {
+                    xml { render annonceInstance as XML}
+                    json { render annonceInstance as JSON }
+                }
+                break
+            case "PUT":
+                break
+            case "PATCH":
+                break
+            case "DELETE":
+                break
+            default:
+                return response.status = 405
+                break
+        }
+        response.status = 406
+    }
+
+    // GET / POST
+    def annonces() {
+        switch(request.getMethod())
+        {
+            case "GET":
+                def annoncesList = Annonce.list()
+                if (!annoncesList)
+                    return response.status = 404
+                response.withFormat {
+                    xml { render annoncesList as XML}
+                    json { render annoncesList as JSON }
+                }
+                break
+            case "POST":
+                break
+            default:
+                return response.status = 405
+                break
+        }
+        response.status = 406
+    }
+}
+
